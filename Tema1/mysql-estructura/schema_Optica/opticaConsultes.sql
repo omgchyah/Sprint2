aefe5d963b4938a7eb84c805809d6838e692475f
Rossana Liendo <rossana.liendo@gmail.com>
2024-05-08 11:39:23 +0200

    Finished optica Diagram

--- Tema1/mysql-estructura/schema_Optica/opticaConsultes.sql
+++ Tema1/mysql-estructura/schema_Optica/opticaConsultes.sql
@@ -1,23 +1,10 @@
 USE cull_dampolla;
 
-/*1*/
-SELECT cliente.nombre,
-COUNT(DISTINCT gafas.id) AS numero_compras
-FROM cliente
-LEFT JOIN gafas ON cliente.id = gafas.cliente_id
-GROUP BY cliente.nombre;
+/*1 Llista el total de compres d’un client/a.*/
+SELECT cliente.nombre, COUNT(DISTINCT gafas.id) AS numero_compras FROM cliente LEFT JOIN gafas ON cliente.id = gafas.id_cliente GROUP BY cliente.nombre;
 
-/*2*/
-SELECT gafas.vendido_por AS nombre_empleado,
-gafas.id AS id_gafas,
-YEAR(gafas.fecha_venta) AS anyo_venta
-FROM gafas
-WHERE YEAR(gafas.fecha_venta) = 2024
-AND gafas.vendido_por LIKE "José";
+/*2 Llista les diferents ulleres que ha venut un empleat durant un any*/
+SELECT empleado.nombre_completo AS nombre_empleado, gafas.id AS id_gafas, YEAR(gafas.fecha_venta) AS anyo_venta FROM gafas LEFT JOIN empleado ON gafas.id_empleado = empleado.id WHERE YEAR(gafas.fecha_venta) = 2024;
 
-/*3*/
-SELECT proveedor.nombre AS nombre_proveedor,
-COUNT(DISTINCT gafas.id) AS gafas_vendidas
-FROM proveedor
-LEFT JOIN gafas ON proveedor.id = gafas.proveedor_id
-GROUP BY nombre_proveedor;
+/*3 Llista els diferents proveïdors que han subministrat ulleres venudes amb èxit per l'òptica.*/
+SELECT proveedor.nombre AS nombre_proveedor, COUNT(DISTINCT gafas.id) AS gafas_vendidas FROM proveedor JOIN gafas ON proveedor.id = gafas.id_proveedor GROUP BY nombre_proveedor;
--- Tema1/mysql-estructura/schema_Optica/schema_Optica.sql
+++ Tema1/mysql-estructura/schema_Optica/schema_Optica.sql
@@ -7,16 +7,15 @@ CREATE TABLE proveedor (
 	nombre VARCHAR(60) NOT NULL,
     calle_nombre VARCHAR(20) NOT NULL,
     calle_numero VARCHAR(20) NOT NULL,
-    piso INT(100) NOT NULL,
-    puerta INT(20) NULL,
-    ciudad VARCHAR(30),
-    codigo_postal VARCHAR(10),
-    pais VARCHAR(20),
+    piso INT NULL,
+    puerta INT NULL,
+    ciudad VARCHAR(30) NOT NULL,
+    codigo_postal VARCHAR(10) NOT NULL,
+    pais VARCHAR(20) NOT NULL,
     telefono_prefijo INT(4) NOT NULL,
     telefono INT(9) NOT NULL,
     fax INT(9) NULL,
-    NIF VARCHAR(9) NOT NULL,
-    INDEX (id)
+    NIF VARCHAR(9) NOT NULL
 );
 
 CREATE TABLE cliente (
@@ -26,10 +25,17 @@ CREATE TABLE cliente (
     telefono INT(9) NOT NULL,
     correo_electronico VARCHAR(40) NOT NULL,
     registro_fecha DATE NOT NULL,
-    recomendacion_id_cliente INT UNSIGNED NULL,
-    FOREIGN KEY (recomendacion_id_cliente) REFERENCES cliente(id),
+    id_cliente_recomendacion INT UNSIGNED NULL,
+    FOREIGN KEY (id_cliente_recomendacion) REFERENCES cliente(id),
     INDEX (id)
     );
+    
+CREATE TABLE empleado (
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre_completo VARCHAR(50) NOT NULL,
+    fecha_inicio DATE NOT NULL
+);
+
     
 CREATE TABLE gafas (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -40,14 +46,17 @@ CREATE TABLE gafas (
     montura_color VARCHAR(20) NOT NULL,
     vidrio_color VARCHAR(20) NOT NULL,
     precio DECIMAL(10, 2) NOT NULL,
-	vendido_por VARCHAR(30) NOT NULL,
     fecha_venta DATE NOT NULL,
-    proveedor_id INT UNSIGNED NOT NULL,
-    cliente_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (proveedor_id) REFERENCES proveedor(id),
-    FOREIGN KEY (cliente_id) REFERENCES cliente(id),
-    INDEX (id)
+	id_empleado INT UNSIGNED NOT NULL,
+    id_proveedor INT UNSIGNED NOT NULL,
+    id_cliente INT UNSIGNED NOT NULL,
+    FOREIGN KEY (id_empleado) REFERENCES empleado(id),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedor(id),
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id),
+    INDEX (id),
+    CONSTRAINT unico_proveedor_marca UNIQUE (marca, id_proveedor)
 );
+
 
 /*DATOS PARA COMPROBAR*/
 
@@ -55,20 +64,25 @@ INSERT INTO proveedor (nombre, calle_nombre, calle_numero, piso, ciudad, codigo_
 VALUES
     ('Ojitos Bonitos', 'Carrer Boronat', '4', 1, 'Barcelona', '08018', 'España', 0034, 123456789, NULL, 'ABC123456'),
     ("Ojoloco", 'Carrer Olzinelles', '35', 2, 'Barcelona', '08014', 'España', 0034, 987654321, NULL, 'DEF987654');
+    
+INSERT INTO empleado (nombre_completo, fecha_inicio)
+VALUES
+	('María Pérez', '2020-04-18'),
+    ('Pol Casses', '2019-05-15');
 
-INSERT INTO cliente (nombre, codigo_postal, telefono, correo_electronico, registro_fecha, recomendacion_id_cliente)
+INSERT INTO cliente (nombre, codigo_postal, telefono, correo_electronico, registro_fecha, id_cliente_recomendacion)
 VALUES
     ('Rossana Liendo', '08950', 610433756, 'rossi@correo.com', '2023-05-01', NULL);
     
-    INSERT INTO cliente (nombre, codigo_postal, telefono, correo_electronico, registro_fecha, recomendacion_id_cliente)
-	VALUES
+INSERT INTO cliente (nombre, codigo_postal, telefono, correo_electronico, registro_fecha, id_cliente_recomendacion)
+VALUES
     ('Pedro Guerrero', '08038', 345879635, 'pedriños@correo.com', '2023-12-02', 1),
     ('Veronica Camacho', '08050', 614587986, 'verocute@correo.com', '2024-01-20', 1),
     ('Vanessa Bermudez', '08450', 689745126, 'vanevanei@correo.com', '2024-05-01', 2);
 
-INSERT INTO gafas (marca, grad_ojo_derecho, grad_ojo_izquierdo, montura_tipo, montura_color, vidrio_color, precio, vendido_por, fecha_venta, proveedor_id, cliente_id)
+INSERT INTO gafas (marca, grad_ojo_derecho, grad_ojo_izquierdo, montura_tipo, montura_color, vidrio_color, precio, fecha_venta, id_empleado, id_proveedor, id_cliente)
 VALUES
-    ('Gucci', 1.25, 1.50, 'flotante', 'Negro', 'Azul', 299.99, 'María', '2023-05-01', 1, 1),
-    ('Chanel', 2.00, 2.25, 'pasta', 'Marrón', 'Verde', 450.60, 'José', '2023-12-02', 2, 2),
-	('Carolina Herrera', 1.80, 2.00, 'metálica', 'Rojo', 'Verde', 350.70, 'José', '2024-01-20', 1, 3),
-    ('Oakley', 4.00, 4.75, 'pasta', 'Negro', 'Amarillo', 280.40, 'José', '2024-05-02', 2, 4);
+    ('Gucci', 1.25, 1.50, 'flotante', 'Negro', 'Azul', 299.99, '2023-05-01', 1, 1, 1),
+    ('Chanel', 2.00, 2.25, 'pasta', 'Marrón', 'Verde', 450.60, '2023-12-02', 1, 2, 2),
+	('Carolina Herrera', 1.80, 2.00, 'metálica', 'Rojo', 'Verde', 350.70, '2024-01-20', 1, 1, 3),
+    ('Oakley', 4.00, 4.75, 'pasta', 'Negro', 'Amarillo', 280.40, '2024-05-02', 2, 2, 4);
